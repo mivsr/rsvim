c3b3c494a2c6dd3dc9fdf0abc2142780276adfe5
zhaochenyou <linrongbin16@gmail.com>
2026-07-04 05:18:32 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -12,7 +12,6 @@ pub mod stat;
 pub mod symlink;
 pub mod write;
 
-use crate::is_v8_int;
 use crate::is_v8_str;
 use crate::js;
 use crate::js::JsRuntime;
@@ -34,88 +33,12 @@ use crate::js::binding::global_rsvim::fs::stat::fs_stat;
 use crate::js::binding::global_rsvim::fs::symlink::FsSymlinkFuture;
 use crate::js::binding::global_rsvim::fs::symlink::FsSymlinkOptions;
 use crate::js::binding::global_rsvim::fs::symlink::fs_symlink;
-use crate::js::binding::global_rsvim::fs::write::FsWriteFuture;
-use crate::js::binding::global_rsvim::fs::write::fs_write;
 use crate::js::converter::*;
 use crate::js::pending;
 use crate::js::resource::ResourceId;
 use crate::prelude::*;
 use itertools::Itertools;
 use std::str::FromStr;
-
-fn _read_file_args<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-) -> String {
-  debug_assert!(args.length() == 1);
-  debug_assert!(is_v8_str!(args.get(0)));
-  let filename = args.get(0).to_rust_string_lossy(scope);
-  trace!("RsvimFs readFile filename:{:?}", filename);
-  filename
-}
-
-/// `Rsvim.fs.readFile` API.
-pub fn read_file<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut rv: v8::ReturnValue,
-) {
-  let filename = _read_file_args(scope, args);
-
-  let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
-  let promise = promise_resolver.get_promise(scope);
-
-  let state_rc = JsRuntime::state(scope);
-  let read_cb = {
-    let promise = v8::Global::new(scope, promise_resolver);
-    let state_rc = state_rc.clone();
-    move |maybe_result: Option<TheResult<Vec<u8>>>| {
-      let fut = FsReadFileFuture {
-        promise: promise.clone(),
-        maybe_result,
-      };
-      let mut state = state_rc.borrow_mut();
-      state.pending_futures.push(Box::new(fut));
-    }
-  };
-
-  let mut state = state_rc.borrow_mut();
-  let task_id = js::TaskId::next();
-  pending::create_fs_read_file(
-    &mut state,
-    task_id,
-    Path::new(&filename),
-    Box::new(read_cb),
-  );
-
-  rv.set(promise.into());
-}
-
-/// `Rsvim.fs.readFileSync` API.
-pub fn read_file_sync<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut rv: v8::ReturnValue,
-) {
-  let filename = _read_file_args(scope, args);
-
-  match fs_read_file(Path::new(&filename)) {
-    Ok(data) => {
-      let buf = v8::ArrayBuffer::new(scope, data.len());
-      let buffer_store = buf.get_backing_store();
-
-      // Copy the slice's bytes into v8's typed-array backing store.
-      for (i, b) in data.iter().enumerate() {
-        buffer_store[i].set(*b);
-      }
-
-      rv.set(buf.into());
-    }
-    Err(e) => {
-      binding::throw_exception(scope, &e);
-    }
-  }
-}
 
 fn _read_dir_args<'s>(
   scope: &mut v8::PinScope<'s, '_>,
--- rsvim_core/src/js/binding/global_rsvim/fs/read_file.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/read_file.rs
@@ -58,3 +58,77 @@ impl JsFuture for FsReadFileFuture {
     self.promise.open(scope).resolve(scope, buf.into()).unwrap();
   }
 }
+
+fn _get_args<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+) -> String {
+  debug_assert!(args.length() == 1);
+  debug_assert!(is_v8_str!(args.get(0)));
+  let filename = args.get(0).to_rust_string_lossy(scope);
+  trace!("RsvimFs readFile filename:{:?}", filename);
+  filename
+}
+
+/// `Rsvim.fs.readFile` API.
+pub fn read_file_async<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+  mut rv: v8::ReturnValue,
+) {
+  let filename = _get_args(scope, args);
+
+  let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
+  let promise = promise_resolver.get_promise(scope);
+
+  let state_rc = JsRuntime::state(scope);
+  let read_cb = {
+    let promise = v8::Global::new(scope, promise_resolver);
+    let state_rc = state_rc.clone();
+    move |maybe_result: Option<TheResult<Vec<u8>>>| {
+      let fut = FsReadFileFuture {
+        promise: promise.clone(),
+        maybe_result,
+      };
+      let mut state = state_rc.borrow_mut();
+      state.pending_futures.push(Box::new(fut));
+    }
+  };
+
+  let mut state = state_rc.borrow_mut();
+  let task_id = js::TaskId::next();
+  pending::create_fs_read_file(
+    &mut state,
+    task_id,
+    Path::new(&filename),
+    Box::new(read_cb),
+  );
+
+  rv.set(promise.into());
+}
+
+/// `Rsvim.fs.readFileSync` API.
+pub fn read_file_sync<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+  mut rv: v8::ReturnValue,
+) {
+  let filename = _get_args(scope, args);
+
+  match fs_read_file(Path::new(&filename)) {
+    Ok(data) => {
+      let buf = v8::ArrayBuffer::new(scope, data.len());
+      let buffer_store = buf.get_backing_store();
+
+      // Copy the slice's bytes into v8's typed-array backing store.
+      for (i, b) in data.iter().enumerate() {
+        buffer_store[i].set(*b);
+      }
+
+      rv.set(buf.into());
+    }
+    Err(e) => {
+      binding::throw_exception(scope, &e);
+    }
+  }
+}
