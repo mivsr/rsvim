c600841c7600e4fb5d4ff67a1ef980e54ba15c95
zhaochenyou <linrongbin16@gmail.com>
2026-07-04 05:26:41 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -21,9 +21,6 @@ use crate::js::binding::global_rsvim::fs::link::fs_link;
 use crate::js::binding::global_rsvim::fs::mkdir::FsMkdirFuture;
 use crate::js::binding::global_rsvim::fs::mkdir::FsMkdirOptions;
 use crate::js::binding::global_rsvim::fs::mkdir::fs_mkdir;
-use crate::js::binding::global_rsvim::fs::stat::FsStatFuture;
-use crate::js::binding::global_rsvim::fs::stat::fs_lstat;
-use crate::js::binding::global_rsvim::fs::stat::fs_stat;
 use crate::js::binding::global_rsvim::fs::symlink::FsSymlinkFuture;
 use crate::js::binding::global_rsvim::fs::symlink::FsSymlinkOptions;
 use crate::js::binding::global_rsvim::fs::symlink::fs_symlink;
@@ -31,84 +28,6 @@ use crate::js::converter::*;
 use crate::js::pending;
 use crate::prelude::*;
 use std::str::FromStr;
-
-fn _symlink_args<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-) -> (
-  /* oldpath */ String,
-  /* newpath */ String,
-  /* options */ FsSymlinkOptions,
-) {
-  debug_assert!(args.length() == 3);
-  debug_assert!(is_v8_str!(args.get(0)));
-  let oldpath = args.get(0).to_rust_string_lossy(scope);
-  debug_assert!(is_v8_str!(args.get(1)));
-  let newpath = args.get(1).to_rust_string_lossy(scope);
-  debug_assert!(is_v8_str!(args.get(2)));
-  let options = args.get(2).to_rust_string_lossy(scope);
-  let options = FsSymlinkOptions::from_str(&options).unwrap();
-  trace!(
-    "RsvimFs symlink oldpath:{:?},newpath:{:?},options:{:?}",
-    oldpath, newpath, options
-  );
-  (oldpath, newpath, options)
-}
-
-/// `Rsvim.fs.symlink` API.
-pub fn symlink<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut rv: v8::ReturnValue,
-) {
-  let (oldpath, newpath, options) = _symlink_args(scope, args);
-
-  let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
-  let promise = promise_resolver.get_promise(scope);
-
-  let state_rc = JsRuntime::state(scope);
-  let link_cb = {
-    let promise = v8::Global::new(scope, promise_resolver);
-    let state_rc = state_rc.clone();
-    move |maybe_result: Option<TheResult<Vec<u8>>>| {
-      let fut = FsSymlinkFuture {
-        promise: promise.clone(),
-        maybe_result,
-      };
-      let mut state = state_rc.borrow_mut();
-      state.pending_futures.push(Box::new(fut));
-    }
-  };
-
-  let mut state = state_rc.borrow_mut();
-  let task_id = js::TaskId::next();
-  pending::create_fs_symlink(
-    &mut state,
-    task_id,
-    Path::new(&oldpath),
-    Path::new(&newpath),
-    options,
-    Box::new(link_cb),
-  );
-
-  rv.set(promise.into());
-}
-
-/// `Rsvim.fs.symlinkSync` API.
-pub fn symlink_sync<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut rv: v8::ReturnValue,
-) {
-  let (oldpath, newpath, options) = _symlink_args(scope, args);
-
-  match fs_symlink(Path::new(&oldpath), Path::new(&newpath), options) {
-    Ok(_) => rv.set_undefined(),
-    Err(e) => {
-      binding::throw_exception(scope, &e);
-    }
-  }
-}
 
 fn _link_args<'s>(
   scope: &mut v8::PinScope<'s, '_>,
