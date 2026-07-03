5bce1640632bbd5368b4b0363a695d5ab9a0e943
zhaochenyou <linrongbin16@gmail.com>
2026-07-04 06:00:31 +0800

    c

--- rsvim_core/src/evloop.rs
+++ rsvim_core/src/evloop.rs
@@ -18,8 +18,8 @@ use crate::hl::ColorSchemeManagerArc;
 use crate::js::JsRuntime;
 use crate::js::JsRuntimeOptions;
 use crate::js::SnapshotData;
-use crate::js::binding::global_rsvim::fs::link::async_fs_link;
-use crate::js::binding::global_rsvim::fs::mkdir::fs_mkdir;
+use crate::js::binding::global_rsvim::fs::link::fs_link_as;
+use crate::js::binding::global_rsvim::fs::mkdir::sync_fs_mkdir;
 use crate::js::binding::global_rsvim::fs::open::async_fs_open;
 use crate::js::binding::global_rsvim::fs::read::fs_read;
 use crate::js::binding::global_rsvim::fs::read_file::async_fs_read_file;
@@ -948,7 +948,7 @@ impl EventLoop {
           let jsrt_forwarder_tx = self.jsrt_forwarder_tx.clone();
           self.detached_tracker.spawn(async move {
             let maybe_result =
-              async_fs_link(req.oldpath.as_path(), req.newpath.as_path()).await;
+              fs_link_as(req.oldpath.as_path(), req.newpath.as_path()).await;
             jsrt_forwarder_tx
               .send(JsMessage::FsLinkResp(chan::FsLinkResp {
                 task_id: req.task_id,
@@ -964,7 +964,7 @@ impl EventLoop {
           trace!("Recv FsMkdirReq:{:?}", req.task_id);
           let jsrt_forwarder_tx = self.jsrt_forwarder_tx.clone();
           self.detached_tracker.spawn_blocking(move || {
-            let maybe_result = fs_mkdir(req.path.as_path(), req.options);
+            let maybe_result = sync_fs_mkdir(req.path.as_path(), req.options);
             jsrt_forwarder_tx
               .send(JsMessage::FsMkdirResp(chan::FsMkdirResp {
                 task_id: req.task_id,
