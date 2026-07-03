0249fe7f6166f121283888c4562d9e75a555f769
zhaochenyou <linrongbin16@gmail.com>
2026-07-04 05:09:57 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -23,9 +23,6 @@ use crate::js::binding::global_rsvim::fs::link::fs_link;
 use crate::js::binding::global_rsvim::fs::mkdir::FsMkdirFuture;
 use crate::js::binding::global_rsvim::fs::mkdir::FsMkdirOptions;
 use crate::js::binding::global_rsvim::fs::mkdir::fs_mkdir;
-use crate::js::binding::global_rsvim::fs::open::FsOpenFuture;
-use crate::js::binding::global_rsvim::fs::open::FsOpenOptions;
-use crate::js::binding::global_rsvim::fs::open::fs_open;
 use crate::js::binding::global_rsvim::fs::read::FsReadFuture;
 use crate::js::binding::global_rsvim::fs::read::fs_read;
 use crate::js::binding::global_rsvim::fs::read_dir::FsReadDirFuture;
@@ -48,100 +45,6 @@ use crate::js::resource::ResourceId;
 use crate::prelude::*;
 use itertools::Itertools;
 use std::str::FromStr;
-
-fn _open_args<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-) -> (/* filename */ String, /* options */ FsOpenOptions) {
-  debug_assert!(args.length() == 2);
-  debug_assert!(is_v8_str!(args.get(0)));
-  let filename = args.get(0).to_rust_string_lossy(scope);
-  debug_assert!(args.get(1).is_object());
-  let options = FsOpenOptions::from_v8(scope, args.get(1));
-  trace!("RsvimFs open filename:{:?},options:{:?}", filename, options);
-  (filename, options)
-}
-
-/// `Rsvim.fs.open` API.
-pub fn open<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut rv: v8::ReturnValue,
-) {
-  let (filename, options) = _open_args(scope, args);
-
-  let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
-  let promise = promise_resolver.get_promise(scope);
-
-  let state_rc = JsRuntime::state(scope);
-  let open_cb = {
-    let promise = v8::Global::new(scope, promise_resolver);
-    let state_rc = state_rc.clone();
-    move |maybe_result: Option<TheResult<Vec<u8>>>| {
-      let fut = FsOpenFuture {
-        promise: promise.clone(),
-        maybe_result,
-      };
-      let mut state = state_rc.borrow_mut();
-      state.pending_futures.push(Box::new(fut));
-    }
-  };
-
-  let mut state = state_rc.borrow_mut();
-  let task_id = js::TaskId::next();
-  let filename = Path::new(&filename);
-  pending::create_fs_open(
-    &mut state,
-    task_id,
-    filename,
-    options,
-    Box::new(open_cb),
-  );
-
-  rv.set(promise.into());
-}
-
-/// `Rsvim.fs.openSync` API.
-pub fn open_sync<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut rv: v8::ReturnValue,
-) {
-  let (filename, options) = _open_args(scope, args);
-
-  let state_rc = JsRuntime::state(scope);
-  let resource_table = state_rc.borrow().resource_table.clone();
-
-  let filename = Path::new(&filename);
-  match fs_open(resource_table, filename, options) {
-    Ok(file_rid) => {
-      let file_rid = Into::<i32>::into(file_rid);
-      let file_rid = file_rid.to_v8(scope);
-      rv.set(file_rid);
-    }
-    Err(e) => {
-      binding::throw_exception(scope, &e);
-    }
-  }
-}
-
-/// `Rsvim.fs.close` API.
-pub fn close<'s>(
-  scope: &mut v8::PinScope<'s, '_>,
-  args: v8::FunctionCallbackArguments<'s>,
-  mut _rv: v8::ReturnValue,
-) {
-  debug_assert!(args.length() == 1);
-  debug_assert!(is_v8_int!(args.get(0)));
-  let file_rid = i32::from_v8(scope, args.get(0));
-  trace!("Rsvim.fs.close:{:?}", file_rid);
-  let file_rid = ResourceId::from(file_rid);
-
-  let state_rc = JsRuntime::state(scope);
-  let resource_table = state_rc.borrow().resource_table.clone();
-
-  fs_close(resource_table, file_rid);
-}
 
 fn _read_args<'s>(
   scope: &mut v8::PinScope<'s, '_>,
--- rsvim_core/src/js/binding/global_rsvim/fs/close.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/close.rs
@@ -1,8 +1,14 @@
 //! Close file APIs.
 
+use crate::is_v8_int;
+use crate::js::JsRuntime;
+use crate::js::converter::*;
 use crate::js::resource::ResourceId;
 use crate::js::resource::ResourceTableArc;
 use crate::prelude::*;
+use crate::prelude::*;
+use itertools::Itertools;
+use std::str::FromStr;
 
 pub fn fs_close(resource_table: ResourceTableArc, rid: ResourceId) {
   let mut resource_table = lock!(resource_table);
@@ -10,4 +16,22 @@ pub fn fs_close(resource_table: ResourceTableArc, rid: ResourceId) {
   debug_assert!(handle.is_some());
   // Drop file handle, i.e. close the file
   handle.take();
+}
+
+/// `Rsvim.fs.close` API.
+pub fn close<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+  mut _rv: v8::ReturnValue,
+) {
+  debug_assert!(args.length() == 1);
+  debug_assert!(is_v8_int!(args.get(0)));
+  let file_rid = i32::from_v8(scope, args.get(0));
+  trace!("Rsvim.fs.close:{:?}", file_rid);
+  let file_rid = ResourceId::from(file_rid);
+
+  let state_rc = JsRuntime::state(scope);
+  let resource_table = state_rc.borrow().resource_table.clone();
+
+  fs_close(resource_table, file_rid);
 }
