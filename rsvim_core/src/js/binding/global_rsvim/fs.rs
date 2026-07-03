89814116c33894d4f9e79f7367f3453a7d93497e
zhaochenyou <linrongbin16@gmail.com>
2026-07-04 06:06:31 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -23,3 +23,5 @@ pub use read::read_async;
 pub use read::read_sync;
 pub use read_dir::read_dir_async;
 pub use read_dir::read_dir_sync;
+pub use read_file::read_file_async;
+pub use read_file::read_file_sync;
