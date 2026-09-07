067b04ff9368b43e51afa7bbf901526dfe6f7001
linrongbin16 <linrongbin16@outlook.com>
2026-09-07 11:47:39 +0800

    build(deps): bump lru to 0.18.4 (#39)

--- rsvim_core/src/js.rs
+++ rsvim_core/src/js.rs
@@ -760,7 +760,7 @@ pub mod boost {
       }
 
       let futures: Vec<Box<dyn JsFuture>> =
-        state_rc.borrow_mut().pending_futures.drain(..).collect();
+        std::mem::take(&mut state_rc.borrow_mut().pending_futures);
       for mut fut in futures {
         fut.run(scope);
         if let Some(exception) = check_exceptions(scope) {
@@ -1164,12 +1164,8 @@ pub fn check_exceptions(scope: &mut v8::PinScope) -> Option<JsError> {
     return Some(error);
   }
 
-  let promise_rejections: Vec<PromiseRejectionEntry> = state_rc
-    .borrow_mut()
-    .exceptions
-    .promise_rejections
-    .drain(..)
-    .collect();
+  let promise_rejections: Vec<PromiseRejectionEntry> =
+    std::mem::take(&mut state_rc.borrow_mut().exceptions.promise_rejections);
 
   // Then, check for unhandled rejections.
   for (promise, exception) in promise_rejections.iter() {
