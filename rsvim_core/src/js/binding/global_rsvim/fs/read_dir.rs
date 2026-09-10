89f0f0b9812d350d69ead2140426996b731c580d
zhaochenyou <linrongbin16@gmail.com>
2026-09-10 16:08:00 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs/read_dir.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/read_dir.rs
@@ -1,5 +1,6 @@
 //! Read directory APIs.
 
+use crate::is_v8_int;
 use crate::is_v8_str;
 use crate::js;
 use crate::js::JsFuture;
@@ -214,4 +215,53 @@ impl JsFuture for FsReadDirNextFuture {
       }
     }
   }
+}
+
+fn _get_next_args<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+) -> ResourceId {
+  debug_assert!(args.length() == 1);
+  debug_assert!(is_v8_int!(args.get(0)));
+  let rid = i32::from_v8(scope, args.get(0));
+  let rid = ResourceId::from(rid);
+  trace!("RsvimFs readDir.next rid:{:?}", rid);
+  rid
+}
+
+/// `Rsvim.fs.readDir` API.
+pub fn read_dir_next_async<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+  mut rv: v8::ReturnValue,
+) {
+  let filename = _get_args(scope, args);
+
+  let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
+  let promise = promise_resolver.get_promise(scope);
+
+  let state_rc = JsRuntime::state(scope);
+  let read_cb = {
+    let promise = v8::Global::new(scope, promise_resolver);
+    let state_rc = state_rc.clone();
+    move |maybe_result: Option<TheResult<Vec<u8>>>| {
+      let fut = FsReadDirFuture {
+        promise: promise.clone(),
+        maybe_result,
+      };
+      let mut state = state_rc.borrow_mut();
+      state.pending_futures.push(Box::new(fut));
+    }
+  };
+
+  let mut state = state_rc.borrow_mut();
+  let task_id = js::TaskId::next();
+  pending::create_fs_read_dir(
+    &mut state,
+    task_id,
+    Path::new(&filename),
+    Box::new(read_cb),
+  );
+
+  rv.set(promise.into());
 }
