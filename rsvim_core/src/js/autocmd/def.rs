ba01a5389b421a763152f26ac808df6e7e198175
zhaochenyou <linrongbin16@gmail.com>
2026-09-15 17:51:44 +0800

    c

--- rsvim_core/src/js/autocmd/def.rs
+++ rsvim_core/src/js/autocmd/def.rs
@@ -1,4 +1,10 @@
 //! Autocmd definition.
+//!
+//! Here are the key difference between traditional Vim:
+//! - We can set an optional name for a "autocmd", the name must be unique just
+//!   like command name.
+//! - We don't have "autocmd group" (I don't feel it is useful), just manage
+//!   all autocmds by IDs/names.
 
 use crate::is_v8_func;
 use crate::is_v8_str;
@@ -21,7 +27,7 @@ pub struct AutoCmdId(#[start_from(1)] i32);
 #[derive(Clone, rsvim_macro::ToV8, rsvim_macro::RcPtr)]
 pub struct AutoCmdDefinition {
   pub id: AutoCmdId,
-  pub name: Option<CompactString>,
+  pub name: Option<CompactString>, // Optional but unique name for a autocmd
   #[derive_where(skip)]
   pub callback: AutoCmdCallback,
   pub attributes: CommandAttributes,
