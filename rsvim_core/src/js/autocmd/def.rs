cebe370e1e38be39cc8430a45bbf1b07e2b7bd55
zhaochenyou <linrongbin16@gmail.com>
2026-09-15 15:04:54 +0800

    c

--- rsvim_core/src/js/autocmd/def.rs
+++ rsvim_core/src/js/autocmd/def.rs
@@ -11,10 +11,17 @@ use std::rc::Rc;
 
 pub type AutoCmdCallback = Rc<v8::Global<v8::Function>>;
 
+// AutoCmdId start from 1.
+#[derive(
+  Copy, Clone, rsvim_macro::IncrementalId, serde::Serialize, serde::Deserialize,
+)]
+pub struct AutoCmdId(#[start_from(1)] usize);
+
 #[derive_where::derive_where(Debug)]
 #[derive(Clone, rsvim_macro::ToV8, rsvim_macro::RcPtr)]
 pub struct AutoCmdDefinition {
-  pub id: CompactString,
+  pub id: AutoCmdId,
+  pub name: Option<CompactString>,
   #[derive_where(skip)]
   pub callback: AutoCmdCallback,
   pub attributes: CommandAttributes,
