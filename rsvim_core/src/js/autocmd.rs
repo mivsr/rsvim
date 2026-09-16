1f66aac1b6c2ca61e0700084b97414f0a1f0e786
zhaochenyou <linrongbin16@gmail.com>
2026-09-16 17:29:40 +0800

    c

--- rsvim_core/src/js/autocmd.rs
+++ rsvim_core/src/js/autocmd.rs
@@ -3,7 +3,6 @@
 pub mod attr;
 pub mod ctx;
 pub mod def;
-pub mod event;
 pub mod opt;
 
 use crate::js::JsFuture;
--- rsvim_core/src/js/autocmd/attr.rs
+++ rsvim_core/src/js/autocmd/attr.rs
@@ -1,7 +1,59 @@
 //! Autocmd attributes.
 
+use crate::js::converter::*;
 use compact_str::CompactString;
 use compact_str::ToCompactString;
+use std::str::FromStr;
+
+#[derive(
+  Debug,
+  Copy,
+  Clone,
+  PartialEq,
+  Eq,
+  PartialOrd,
+  Ord,
+  Hash,
+  strum_macros::Display,
+  strum_macros::EnumString,
+)]
+pub enum AutoCmdEvent {
+  #[strum(serialize = "VimEnter")]
+  /// After editor enter
+  VimEnter,
+
+  #[strum(serialize = "VimLeave")]
+  /// Just before editor exit
+  VimLeave,
+
+  #[strum(serialize = "VimLeavePre")]
+  /// Just before all exit operations before editor exit
+  VimLeavePre,
+
+  #[strum(serialize = "VimResized")]
+  /// After editor resized
+  VimResized,
+}
+
+impl FromV8 for AutoCmdEvent {
+  fn from_v8<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    value: v8::Local<'s, v8::Value>,
+  ) -> Self {
+    debug_assert!(value.is_string() || value.is_string_object());
+    let event = value.to_string(scope).unwrap().to_rust_string_lossy(scope);
+    AutoCmdEvent::from_str(&event).unwrap()
+  }
+}
+
+impl ToV8 for AutoCmdEvent {
+  fn to_v8<'s>(
+    &self,
+    scope: &mut v8::PinScope<'s, '_>,
+  ) -> v8::Local<'s, v8::Value> {
+    self.to_string().to_v8(scope)
+  }
+}
 
 #[derive(
   Debug,
@@ -13,8 +65,8 @@ use compact_str::ToCompactString;
   rsvim_macro::FromV8,
 )]
 pub struct CommandAttributes {
-  #[builder(default = "".to_compact_string())]
-  pub event: CompactString,
+  #[builder]
+  pub event: AutoCmdEvent,
 
   #[builder(default = None)]
   pub pattern: Option<CompactString>,
